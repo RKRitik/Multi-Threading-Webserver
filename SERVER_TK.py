a7cd9c6f1fbed21f65c9c5421ced011ecef5f0fd
AntonyPClement <46244407+AntonyPClement@users.noreply.github.com>
2019-11-03 22:05:06 +0530

    Update SERVER_TK.py

--- SERVER_TK.py
+++ SERVER_TK.py
@@ -34,19 +34,15 @@ class ClientHandler(Thread):
                     imgFiles = os.listdir(sb+'\''+au)
                     images = [img for img in imgFiles if img.endswith('.jpg')]
                     print ('\n%s user done' ,au)"""
-        #self._client.send(file)
+        
 
-        #copies all .img files in the folder from server to client
-        #for imgs in images:
-        #self._client.send(file)
         file_name = open(file,'rb')
         while True:
             strng = file_name.readline()
             if not strng:
                 break
             self._client.send(strng)
-        file_name.close()
-        #os.remove(sb+'/'+au+'/'+imgs)       
+        file_name.close()       
         print ("Data sent successfully")                          
         time.sleep(1)
 
@@ -62,11 +58,8 @@ s.listen(5)
 executor = ThreadPoolExecutor(max_workers=5)
 while True:
     print('Waiting for connection')
-    #lab=Label(root, text="Waiting for connection").pack()
     client, address = s.accept()
 
     executor.submit(ClientHandler(client,address).start())
     # handler = ClientHandler(client)
     # handler.start()
-
-#root.mainloop()
